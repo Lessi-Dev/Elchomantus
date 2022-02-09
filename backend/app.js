c6e722682d69328cc8839f098273a797f96fa388
Lessi-Dev <Leo.boerl2006@gmail.com>
2022-02-09 23:43:08 +0100

    added security to user creation

--- backend/app.js
+++ backend/app.js
@@ -78,11 +78,27 @@ app.get('/setToken/:token', (req, res) => {
   });
 })
 
-app.get('/createUser/:name/:tag', (req, res) => {
+app.get('/createTrackedUser/:name/:tag',async (req, res) => {
+  if (req.params.name != undefined && req.params.tag != undefined) {
+  const usr = await User.findOne({name: req.params.name, tag: req.params.tag}, (err, user) => {
+    if (err) {
+      console.log("Error:"+err);
+      return    }
+    res.send(user);
+  }).catch(err => {console.log(err)})
+  if(usr != null) {
+    console.log("User not found")
     User.create({name: req.params.name, tag: req.params.tag}, (err, user) => {
-      if (err) return console.log(err);
+      if (err){
+        res.send(err);
+        return  
+      } 
       res.send(user.name);
     });
+  }
+  } else {
+    res.send('Missing parameters');
+  }
 });
 
 function UpdateUser() {
